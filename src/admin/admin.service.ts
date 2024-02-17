46bfbe1196ed830997a7cabf865891bf3777f9dd
mmtz <mohmohtheinzaw379@gmail.com>
2024-02-17 12:31:15 +0630

    admi crud init

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { BadRequestException } from 'src/response/response';
+import { IResponse } from 'src/response/response-interfaces';
 
 @Injectable()
 export class AdminService {
@@ -7,14 +9,34 @@ export class AdminService {
         private readonly dbService:PrismaService
     ){}
 
-    async findOne(id:string){
+    async findOne(id:string):Promise<IResponse>{
         try {
-            const admin = await this.dbService.admin.findUnique({
+            const admin = await this.dbService.admin.findUniqueOrThrow({
                 where:{
                     id
                 }
             })
-            return admin
+            return {
+                data:admin,
+                metadata:{
+                    message:'fetch admin successfully.',
+                    statusCode:HttpStatus.OK
+                }
+            }
+        } catch (error) {
+            throw new BadRequestException({
+                message:'fail to fetch admin',
+                code:HttpStatus.BAD_REQUEST,
+                cause:new Error(error)
+            })
+        }
+    }
+
+    async fetchAll():Promise<IResponse>{
+        try {
+            const data = await this.dbService.admin.findMany({
+                
+            })
         } catch (error) {
             
         }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { RegisterAdmin } from './dto/admin.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+@ApiTags('Admin')
+export class AuthController {
+    private readonly authService:AuthService
+    @Post('')
+    @ApiBody({type:RegisterAdmin})
+    async register(@Body() dto:RegisterAdmin){
+        return this.authService.registerAdmin(dto)
+    }
+}
